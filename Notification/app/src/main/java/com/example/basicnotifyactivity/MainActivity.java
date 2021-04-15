7bb1f8d378f9cfa5b4b62b76b5beb03d91d2b9e9
Thomas-Wei-666 <72587206+Thomas-Wei-666@users.noreply.github.com>
2021-04-15 21:53:51 +0800

    v1.0 实现了基本的通知发送 注意：现在需要在build那一大串属性之前先开辟一个channel 判断版本->开辟->用build设置属性（记得一定要设置对应的channel id）

--- Notification/app/src/main/java/com/example/basicnotifyactivity/MainActivity.java
+++ Notification/app/src/main/java/com/example/basicnotifyactivity/MainActivity.java
@@ -21,22 +21,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btSendNotification = (Button)findViewById(R.id.bt_send_notification);
+        Button btSendNotification = (Button) findViewById(R.id.bt_send_notification);
         btSendNotification.setOnClickListener(v -> {
             sendNotify(MainActivity.this);
         });
     }
-    private void sendNotify(Context context){
-        NotificationManager manager =(NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O) {//需要判断是否是androidO(8.0)以上的系统
+
+    private void sendNotify(Context context) {
+        NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {//需要判断是否是androidO(8.0)以上的系统
             NotificationChannel channel = new NotificationChannel("001", "myChannel001", NotificationManager.IMPORTANCE_DEFAULT);//channel id 很重要！
             manager.createNotificationChannel(channel);
         }
-        Intent intent = new Intent(context,NotificationActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context,0,intent,0);
-        Notification notification = new NotificationCompat.Builder(context,"001").setContentTitle("Congratulations").
+        Intent intent = new Intent(context, NotificationActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        Notification notification = new NotificationCompat.Builder(context, "001").setContentTitle("Congratulations").
                 setContentText("you have successfully send a notification!").setContentIntent(pendingIntent).setSmallIcon(R.mipmap.ic_launcher)
-                .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher)).setAutoCancel(true).setChannelId("001").build();//必须设置channel id不然不显示
-        manager.notify(1,notification);
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher)).setAutoCancel(true).setChannelId("001").build();//必须设置channel id不然不显示
+        manager.notify(1, notification);
     }
 }
